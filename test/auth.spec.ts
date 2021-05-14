49c1bfe27159347d962d99326043a03d40f9a0db
dilrong <wet932@naver.com>
2021-05-14 19:14:17 +0900

    Add: TestCode#8 auth.spec outline

---
+++ test/auth.spec.ts
@@ -0,0 +1,31 @@
+
+describe('User Service unit test', () => {
+
+    describe('User is to be SignIn', () => {
+        test('Should signIn user return token data', async() => {
+
+        });
+
+        test('Should signIn user return member data', async() => {
+
+        });
+    });
+
+    describe('User is to be SignUp', () => {
+        test('Should SignUp user return success message', async() => {
+
+        });
+    });
+
+    describe('User is to be email confirm.', () => {
+        test('Should email confirm user return success message', async() => {
+
+        });
+    });
+
+    describe('User is to be Modify personal information.', () => {
+        test('Should Modify personal information. user return success message', async() => {
+
+        });
+    });
+});
