a0d80cf14695e03727cc1793ae7cb6dc15f4b435
dilrong <wet932@naver.com>
2021-05-14 19:16:19 +0900

    Update: TestCode Outline#8 Add expect outline

--- test/auth.spec.ts
+++ test/auth.spec.ts
@@ -3,7 +3,9 @@ describe('User Service unit test', () => {
 
     describe('User is to be SignIn', () => {
         test('Should signIn user return token data', async() => {
+            const message = '';
 
+            expect(message).toEqual('success');
         });
 
         test('Should signIn user return member data', async() => {
@@ -13,19 +15,25 @@ describe('User Service unit test', () => {
 
     describe('User is to be SignUp', () => {
         test('Should SignUp user return success message', async() => {
+            const message = '';
 
+            expect(message).toEqual('success');
         });
     });
 
     describe('User is to be email confirm.', () => {
         test('Should email confirm user return success message', async() => {
+            const message = '';
 
+            expect(message).toEqual('success');
         });
     });
 
     describe('User is to be Modify personal information.', () => {
         test('Should Modify personal information. user return success message', async() => {
+            const message = '';
 
+            expect(message).toEqual('success');
         });
     });
 });
