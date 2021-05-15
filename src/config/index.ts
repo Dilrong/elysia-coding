1b6ddbca0d0ca4af55bf85eb6e64e74020cc694e
dilrong <wet932@naver.com>
2021-05-15 10:04:03 +0900

    Add: DB config

---
+++ src/config/index.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+    port: process.env.PORT,
+    env: process.env.NODE_ENV,
+    jwt: {
+      key: process.env.JWT_KEY
+    },
+    productDB: {
+      name: process.env.DB_PRO_NAME,
+      user: process.env.DB_PRO_USER,
+      password: process.env.DB_PRO_PASSWORD,
+      host: process.env.DB_PRO_HOST,
+      port: process.env.DB_PRO_PORT
+    },
+    developmentDB: {
+      name: process.env.DB_DEV_NAME,
+      user: process.env.DB_DEV_USER,
+      password: process.env.DB_DEV_PASSWORD,
+      host: process.env.DB_DEV_HOST,
+      port: process.env.DB_DEV_PORT
+    },
+    localDB: {
+        name: process.env.DB_LOCAL_NAME,
+        user: process.env.DB_LOCAL_USER,
+        password: process.env.DB_LOCAL_PASSWORD,
+        host: process.env.DB_LOCAL_HOST,
+        port: process.env.DB_LOCAL_PORT
+    }
+}
